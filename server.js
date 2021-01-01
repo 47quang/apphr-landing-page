7cd673dd042f86343850beff03e3748e0b840a9f
tran thanh quang <quang.tran290699@gmail.com>
2021-01-01 18:13:04 +0700

    feat: create server

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const public_path = path.join(__dirname, 'build');
+const port = process.env.PORT || 3000;
+
+console.log('---- Public path: ', public_path);
+app.use(express.static(public_path));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(public_path, 'index.html'));
+});
+app.get('*', (req, res) => {                       
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port, () => {
+   console.log('Server is up!');
+});
